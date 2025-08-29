ae82f42f7501c7480660fe86d5212235b338c61b
Dalia Osama <dalyaasamh19@gmail.com>
2025-08-29 07:18:31 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+public class MainClass {
+    public static void main(String[] args) {
+    // (2) construct a MyClass object called myObject
+    // (3) call greetings()
+    // (4) call update method
+}
+}
+
+class MyClass {
+    // (1) definition of MyClass constructor
+
+    public void greetings() {
+        // (5) a function that prints the greeting
+    }
+
+    public void update(int num, String title) {
+    // (6)
+}
+
+public void print() {
+    // (7)
+}
+
+private int numOfItems;
+private String reportTitle;
+}
